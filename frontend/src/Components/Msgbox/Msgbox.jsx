7b5073aa8d4e2b43e6fef240b7ce7adc0aedfc34
ANANTHAKRISHNAN-1234 <ananthakrishnan829@gmail.com>
2024-12-29 17:00:59 +0530

    front end and backend connected

--- frontend/src/Components/Msgbox/Msgbox.jsx
+++ frontend/src/Components/Msgbox/Msgbox.jsx
@@ -1,7 +1,36 @@
-import React, { useState, useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import "./Msgbox.css";
 function Msgbox({ messages, setMessages }) {
   const textareaRef = useRef(null);
+  const handleBackendRequest = async (e) => {
+    const response = await fetch("http://127.0.0.1:5000/get_data", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify({ question: messages[messages.length - 1].msg }),
+    });
+
+    if (!response.ok) {
+      console.error("No response bruh!");
+    }
+
+    console.log(response.status + "successs ahda monu");
+
+    const data = await response.json();
+    //setResponseData(data['output']);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { msg: data["output"], isbot: true },
+    ]);
+    //speakText(data['output'])
+  };
+  useEffect(() => {
+    if (messages.length > 0 && messages.length % 2) {
+      console.log("end message: " + messages[messages.length - 1]);
+      handleBackendRequest();
+    }
+  }, [messages]);
   const sendMsg = () => {
     const msg = textareaRef.current.value;
     if (msg !== "") {
